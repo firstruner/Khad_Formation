ba065665eaaa7dc7361bcf2c4c92bb69163974d1
Chris Fixe Maison <cboulas@msn.com>
2019-10-19 19:23:59 +0200

    Initialize

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // --- Projet : KhadLib1 ---
+            // TODO 1 : Créer une classe instanciable héritant de l'interface
+            //          IEncoreDecode permettant d'encrypter et décrypter une chaine
+            // 
+            // TODO 2 : Créer une méthode permettant l'accès à la classe créer précédemment via une factory
+            //
+            // --- Projet : ConsoleApp1
+            // TODO : Encrypter un texte via la clé publique fournie dans l'énumération de l'app un texte et
+            //        sauvegarder le texte crypter (J'ai la clé privée - Si tout va bien je pourrais décrypter ton texte)
+            //
+            // TODO : Encrypter le texte fournie dans l'énumération de l'app avec la clé de l'app et le sauvegarder
+            //        dans un fichier
+
+            // !! ATTENTION à ta façon de coder !!
+        }
+    }
+}
