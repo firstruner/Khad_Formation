9b858fb09dfeb83b7214311e6929a39383d36345
Christophe_Informatique <cboulas@msn.com>
2019-10-24 13:20:42 +0200

    Travail à faire - 2019/10/24

---
+++ ConsoleApp1/KhadLib1/Classes/People.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KhadLib1.Classes
+{
+    public class People
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public TimeSpan Age => DateTime.Now.Subtract(Birthdate);
+
+        public People() { }
+    }
+}
